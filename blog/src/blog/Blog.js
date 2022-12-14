592c9206e6c510b73958a07ddad1dbc0a32a3827
kimmose1992 <kimmose1992@gmail.com>
2022-12-14 17:58:16 +0900

    Modal View Function

--- blog/src/blog/Blog.js
+++ blog/src/blog/Blog.js
@@ -3,9 +3,10 @@ import React, { useState } from "react";
 
 function Blog() {
 
-  let [blogTitle, setBlogTitle] = useState('React Blog');
+  let [blogTitle] = useState('React Blog');
   let [listTitle, setListTitle] = useState(['List Title 03', 'List Title 02', 'List Title 01']);
   let [likeCnt, setLikeCnt] = useState([0, 0, 0]);
+  let [isModal, setIsModal] = useState(false);
 
   /*
   const sortListTitle = () => {
@@ -19,11 +20,11 @@ function Blog() {
     copyLikeCnt[idx]++;
     setLikeCnt(copyLikeCnt); 
   }
-  */
   
   const changeBlogTitle = () => {
     setBlogTitle(preBlogTitle => preBlogTitle + ' Change ');
   }
+  */  
 
   function addLikeCnt(idx) {
     let copyLikeCnt =  [...likeCnt];
@@ -36,40 +37,54 @@ function Blog() {
     copyListTitle.sort();
     setListTitle(copyListTitle);
   }
-  
+
+  function blogModalView() {
+    //console.log(isModal);
+    //let copyIsModal = [...isModal];
+    //copyIsModal = isModal ? false : true;
+    setIsModal(isModal ? false : true);
+  }
+
   return (
     <div className="Blog">
 
       { /* Blog Header */ }
-      <div className="blog-nav">
-        <h4>
-          <span onClick={ changeBlogTitle }>{ blogTitle }</span> 
-          <span style={{ color: 'white' }} onClick={() => { sortListTitle() }}> ✅Sort</span>
+      <div className="Blog-nav">
+        <h4>{ blogTitle } <span style={{ color: 'white' }} onClick={() => { sortListTitle() }}> ✅Sort</span>
         </h4>        
       </div>
 
       { /* Blog List */ }
-      <div className='blog-list'>
-        <h4>{ listTitle[0] } <span onClick={() => { addLikeCnt(0) }}>👍</span> { likeCnt[0] } </h4>
+      <div className='Blog-list'>
+        <h4>
+          <span onClick={ blogModalView }>{ listTitle[0] }</span> 
+          <span onClick={() => { addLikeCnt(0) }}>👍</span> { likeCnt[0] } 
+        </h4>
         <p>2022.12.12</p>        
       </div>
-      <div className='blog-list'>
+      <div className='Blog-list'>
         <h4>{ listTitle[1] } <span onClick={ () => { addLikeCnt(1) }}>👍</span> { likeCnt[1] } </h4>
         <p>2022.12.12</p>        
       </div>
-      <div className='blog-list'>
+      <div className='Blog-list'>
         <h4>{ listTitle[2] } <span onClick={ () => { addLikeCnt(2) }}>👍</span> { likeCnt[2] } </h4>
         <p>2022.12.12</p>        
       </div>
 
       { /* Blog Modal */ }
-      <div className='blog-modal'>
-        <h4>제목</h4>
-        <p>내용</p>
-        <p>등록일자</p>
-      </div>      
+      { isModal ? <BlogModal /> : null }
     </div>
   );
+}
+
+function BlogModal() {
+  return (
+    <div className='Blog-modal'>
+      <h4>제목</h4>
+      <p>내용</p>
+      <p>등록일자</p>      
+    </div>
+  )
 }
 
 export default Blog;
