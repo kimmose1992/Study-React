d00e24daf3de0f03ca992dd83e233db5f9a1e56f
kimmose1992 <kimmose1992@gmail.com>
2022-12-15 13:23:04 +0900

    StrictMode comment

--- blog/src/index.js
+++ blog/src/index.js
@@ -6,6 +6,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  /* 
+   * <React.StrictMode>
+   *
+   * React에서 사용하는 잠재적 문제를 검사하기 위한 도구 (일반 JS에선 use strict 가 있음)
+   * 정확한 검사를 위해 랜더링을 2번 수행한다. 
+   * ※ console 등, 2번 수행되는 경우 해당 모드를 주석처리하면 된다.
+   */
   <React.StrictMode>
     <App />
   </React.StrictMode>
